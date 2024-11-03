18bb68b3c9125de77193906d4fe5c3503f1fc1a1
Tomer <tomermi123@gmail.com>
2024-11-03 13:42:53 +0200

    feat: Register PythonController and PythonExecutorService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PythonController } from './YahooClient/python/python.controller';
+import { PythonExecutorService } from './YahooClient/python-executor/python-executor.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PythonController],
+  providers: [AppService, PythonExecutorService],
 })
 export class AppModule {}
