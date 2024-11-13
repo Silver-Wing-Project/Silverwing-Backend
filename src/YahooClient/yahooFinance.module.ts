18454a3026fbec54beb2ff04d72449cf6c179ed8
Tomer <tomermi123@gmail.com>
2024-11-13 17:58:34 +0200

    feat: Add YahooFinanceModule, a simple test, and package initialization for YahooClient, along with data in csv files

---
+++ src/YahooClient/yahooFinance.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StockPrice, StockPriceSchema } from './schemas/stock-price.schema';
+import { StockReport, StockReportSchema } from './schemas/stock-report.schema';
+import { PythonExecutorService } from './python/python-executor.service';
+import { PythonController } from './python/python.controller';
+import { FinanceService } from './services/finance.service';
+import { FinanceController } from './controllers/finance.controller';
+import { TestStockService } from './services/test-stock.service';
+import { TestStockController } from './controllers/test-stock.controller';
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: StockPrice.name, schema: StockPriceSchema },
+      { name: StockReport.name, schema: StockReportSchema },
+    ]),
+  ],
+  controllers: [PythonController, TestStockController, FinanceController],
+  providers: [PythonExecutorService, TestStockService, FinanceService],
+})
+export class YahooFinanceModule {}
