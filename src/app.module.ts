5843c4a81c087dfe57ee4f1b4f6dc1b6cb057fdf
Tomer <tomermi123@gmail.com>
2024-11-13 17:59:35 +0200

    feat: Integrate Mongoose and YahooFinanceModule, alongside updating package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PythonController } from './YahooClient/python/python.controller';
-import { PythonExecutorService } from './YahooClient/python-executor/python-executor.service';
+import { PythonExecutorService } from './YahooClient/python/python-executor.service';
+import { YahooFinanceModule } from './YahooClient/yahooFinance.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/silverwing'),
+    YahooFinanceModule,
+  ],
   controllers: [AppController, PythonController],
   providers: [AppService, PythonExecutorService],
 })
