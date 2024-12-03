7cb1ddc3a4f197c97f2db55b78af30cc7fed547d
Tomer <tomermi123@gmail.com>
2024-12-03 14:40:05 +0200

    feat: Integrate StockController and StockService into YahooFinanceModule. Removing unused python service and controller.

--- src/YahooClient/yahooFinance.module.ts
+++ src/YahooClient/yahooFinance.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StockPrice, StockPriceSchema } from './schemas/stock-price.schema';
-import { StockReport, StockReportSchema } from './schemas/stock-report.schema';
 import { PythonExecutorService } from './python/python-executor.service';
 import { FinanceService } from './services/finance.service';
 import { FinanceController } from './controllers/finance.controller';
+import { StockPrice, StockPriceSchema } from './schemas/stock-price.schema';
+import { StockReport, StockReportSchema } from './schemas/stock-report.schema';
 import { StockPriceRepository } from './repositories/stock-price.repository';
 import { StockReportRepository } from './repositories/stock-report.repository';
+import { StockController } from './controllers/stock.controller';
+import { StockService } from './services/stock.service';
 
 @Module({
   imports: [
@@ -15,12 +17,13 @@ import { StockReportRepository } from './repositories/stock-report.repository';
       { name: StockReport.name, schema: StockReportSchema },
     ]),
   ],
-  controllers: [FinanceController],
+  controllers: [FinanceController, StockController],
   providers: [
     PythonExecutorService,
     FinanceService,
     StockPriceRepository,
     StockReportRepository,
+    StockService,
   ],
 })
 export class YahooFinanceModule {}
