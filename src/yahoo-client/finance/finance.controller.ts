70e485daff11e49f51c11146a789fedbd5aa8871
Tomer <tomermi123@gmail.com>
2025-01-02 16:05:55 +0200

    feat: Add Finance module with a controller that uses 2 endpoints: for stock-price and stock-report

---
+++ src/yahoo-client/finance/finance.controller.ts
@@ -0,0 +1,129 @@
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Query,
+  UseFilters,
+} from '@nestjs/common';
+
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBody,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+import { AllExceptionsFilter } from '../utility/filters/all-exceptions.filter';
+
+import { PythonService } from '../utility/ts-services/python.service';
+import { StockReportService } from '../stock/stock-report/stock-report.service';
+import { StockPriceService } from './../stock/stock-price/stock-price.service';
+import { StockPrice } from '../stock/stock-price/entities/stock-price.schema';
+import { StockReport } from '../stock/stock-report/entities/stock-report.schema';
+import { parseStockPricesData, parseStockReportsData } from '../utility/data-parsers/data-parser.utils';
+
+
+@ApiTags('Finance')
+@Controller('finance')
+@UseFilters(AllExceptionsFilter)
+export class FinanceController {
+  constructor(
+    private readonly pythonService: PythonService,
+    private readonly stockPriceService: StockPriceService,
+    private readonly stockReportService: StockReportService,
+  ) {}
+
+  @Get('fetch-stock-prices/')
+  @ApiOperation({ summary: 'Fetch stock prices for a given ticker and date range'})
+  @ApiParam({ name: 'ticker', type: String, description: 'Stock Ticker' })
+  @ApiParam({ name: 'startDate', type: String, description: 'Start Date' })
+  @ApiParam({ name: 'endDate', type: String, description: 'End Date' })
+  @ApiResponse({ status: 200, description: 'The stock prices have been successfully fetched.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 404, description: 'Stock prices not found.' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+  async fetchStockPrices(
+    @Query('ticker') ticker: string,
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ): Promise<StockPrice[]> {
+    const existingPrices = await this.stockPriceService.findManyStockPrices(
+      ticker,
+      startDate,
+      endDate,
+    );
+    if (existingPrices.length > 0) {
+      return existingPrices;
+    }
+
+    const stockPricesData = await this.pythonService.fetchStockData(
+      ticker,
+      startDate,
+      endDate,
+    );
+
+    if (!Array.isArray(stockPricesData)) {
+      throw new InternalServerErrorException('Invalid data format received from Python service');
+    }
+
+    const parsedStockPricesData = parseStockPricesData(stockPricesData);
+    const stockPrices = await this.stockPriceService.createManyStockPrices(parsedStockPricesData);
+    return stockPrices;
+  }
+
+  @Get('price/')
+  @ApiOperation({ summary: 'Get a stock price by ID' })
+  @ApiParam({ name: '_id', type: String, description: 'Stock Price ID' })
+  @ApiBody({ type: StockPrice })
+  @ApiResponse({ status: 200, description: 'The stock price has been successfully fetched.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 404, description: 'Stock price not found.' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+  async findStockPriceById(@Query('_id') _id: string): Promise<StockPrice> {
+    return await this.stockPriceService.findStockPriceById(_id);
+  }
+
+
+  @Get('fetch-stock-reports/')
+  @ApiOperation({ summary: 'Fetch stock reports for a given ticker and report type' })
+  @ApiParam({ name: 'ticker', type: String, description: 'Stock Ticker' })
+  @ApiParam({ name: 'reportType', type: String, description: 'Report Type' })
+  @ApiResponse({ status: 200, description: 'The stock reports have been successfully fetched.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+  async fetchStockReports(
+    @Query('ticker') ticker: string,
+    @Query('reportType') reportType: string,
+  ): Promise<StockReport[]> {
+    const existingReports = await this.stockReportService.findManyStockReports(
+      ticker,
+      reportType,
+    );
+    if (existingReports.length > 0) {
+      return existingReports;
+    }
+
+    const stockReportsData = await this.pythonService.fetchStockReports(
+      ticker,
+      reportType,
+    );
+
+    const parsedStockReportsData = parseStockReportsData(stockReportsData);
+    const stockReports = await this.stockReportService.createManyStockReports(parsedStockReportsData);
+    return stockReports;
+  }
+ 
+
+  @Get('report/:_id')
+  @ApiOperation({ summary: 'Get a stock report by ID' })
+  @ApiParam({ name: '_id', type: String, description: 'Stock Report ID' })
+  @ApiBody({ type: StockReport })
+  @ApiResponse({ status: 200, description: 'The stock report has been successfully fetched.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 404, description: 'Stock report not found.' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+  async findStockReportById(@Query('_id') _id: string): Promise<StockReport> {
+    return await this.stockReportService.findStockReportById(_id);
+  }
+}
