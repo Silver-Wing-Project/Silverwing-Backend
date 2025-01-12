34d0589ba04a9fbf6b8bcdcc5ba08f073f987743
Tomer <tomermi123@gmail.com>
2025-01-12 19:05:39 +0200

    feat: Enhance UpdateStockReportDto with validation and type improvements

--- src/yahoo-client/stock/stock-report/dto/update-stock-report.dto.ts
+++ src/yahoo-client/stock/stock-report/dto/update-stock-report.dto.ts
@@ -1,20 +1,25 @@
-import { IsString, IsDate } from 'class-validator';
+import { IsString, IsDate, IsNotEmpty, IsObject } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateStockReportDto {
   @IsString()
+  @IsNotEmpty()
   _id: string;
 
   @IsString()
+  @IsNotEmpty()
   ticker: string;
 
   @IsDate()
+  @IsNotEmpty()
   @Type(() => Date)
   date: Date;
 
   @IsString()
+  @IsNotEmpty()
   reportType: string;
 
-  @IsString()
-  content: string;
+  @IsObject()
+  @IsNotEmpty()
+  content: Record<string, any>;
 }
