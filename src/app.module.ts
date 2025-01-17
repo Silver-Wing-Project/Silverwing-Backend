a07f958ab0b62cec00ed87cb08f7ab3be1941da2
Tomer <tomermi123@gmail.com>
2025-01-17 17:36:46 +0200

    feat: Add .dockerignore, update .gitignore, and integrate @nestjs/config for environment variable management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,8 @@ import { StockModule } from './yahoo-client/stock/stock.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/silverwing'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     FinanceModule,
     StockModule,
   ],
