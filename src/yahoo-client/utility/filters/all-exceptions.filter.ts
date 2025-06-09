09863b5b68551b46031745c57436587e7ad290d0
Tomer <tomermi123@gmail.com>
2025-06-09 15:28:11 +0300

    feat: Enhance AllExceptionsFilter with logging and improved response structure

--- src/yahoo-client/utility/filters/all-exceptions.filter.ts
+++ src/yahoo-client/utility/filters/all-exceptions.filter.ts
@@ -1,10 +1,16 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 import { Request, Response } from 'express';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
   catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
+
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
@@ -16,11 +22,15 @@ export class AllExceptionsFilter implements ExceptionFilter {
           : (exception.getResponse() as any).message || exception.message
         : 'Internal server error';
 
-    response.status(status).json({
+    const responseBody = {
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
       message,
-    });
+    };
+
+    this.logger.error(`unhandled exception: ${message}`, (exception as any)?.stack);
+
+    httpAdapter.reply(response, responseBody, status);
   }
 }
