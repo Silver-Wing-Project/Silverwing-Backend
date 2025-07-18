1b7b637c2ea9c688fdbf1997a0ecc6ffdad3019e
Tomer <tomermi123@gmail.com>
2025-07-18 13:15:04 +0300

    feat: Update UtilityModule to include DataCompletenessService and StockDataOrchestrationService with corresponding providers

--- src/yahoo-client/utility/utility.module.ts
+++ src/yahoo-client/utility/utility.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { PythonExecutorService } from '@python-executor/python-executor.service';
+import { PythonExecutorService } from './python-executor/python-executor.service';
 import { PythonService } from '@ts-services/python.service';
-import { DateRangeService } from './ts-services/date-range/date-range.service';
+import { DateRangeService } from '@ts-services/date-range/date-range.service';
+import { DataCompletenessService } from '@ts-services/data-completeness/data-completeness.service';
+import { StockDataOrchestrationService } from '@ts-services/stock-data-orchestration/stock-data-orchestration.service';
 
 @Module({
-  providers: [PythonExecutorService, PythonService, DateRangeService],
-  exports: [PythonExecutorService, PythonService, DateRangeService],
+  providers: [
+    PythonExecutorService,
+    PythonService,
+    DateRangeService,
+    DataCompletenessService,
+    StockDataOrchestrationService,
+    {
+      provide: 'IDateRangeService',
+      useExisting: DateRangeService,
+    },
+    {
+      provide: 'IDataCompletenessService',
+      useExisting: DataCompletenessService,
+    },
+    {
+      provide: 'IStockDataOrchestrationService',
+      useExisting: StockDataOrchestrationService,
+    },
+  ],
+  exports: [
+    PythonExecutorService,
+    PythonService,
+    DateRangeService,
+    DataCompletenessService,
+    StockDataOrchestrationService,
+    // 'IDateRangeService',
+    // 'IDataCompletenessService',
+    // 'IStockDataOrchestrationService',
+  ],
 })
 export class UtilityModule {}
