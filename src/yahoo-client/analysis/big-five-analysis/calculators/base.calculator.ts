0acfe7e1cc37f6004d538536e2c329f2c0cc2eae
Tomer <tomermi123@gmail.com>
2025-10-25 22:49:20 +0300

    feat: Initialization  of BaseCalculator class

---
+++ src/yahoo-client/analysis/big-five-analysis/calculators/base.calculator.ts
@@ -0,0 +1,11 @@
+import { GrowthRates, YearValue } from '../interfaces/big-five.interface';
+
+/**
+ * Base Calculator class with common utility methods for Big Five analysis calculators.
+ */
+export class BaseCalculator {
+  /**
+   * Phil Town's recommended minimum growth rate
+   */
+  protected static readonly PHIL_TOWN_THRESHOLD = 10;
+}
