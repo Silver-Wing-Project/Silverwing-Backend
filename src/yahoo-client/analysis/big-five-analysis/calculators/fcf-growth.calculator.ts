07250439a40d89d1d9b7c1c5f6d611ed6012a3f9
Tomer <tomermi123@gmail.com>
2025-10-27 12:21:26 +0200

    feat: Implement methods to get Free Cash Flow growth rate

--- src/yahoo-client/analysis/big-five-analysis/calculators/fcf-growth.calculator.ts
+++ src/yahoo-client/analysis/big-five-analysis/calculators/fcf-growth.calculator.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CashFlowData, GrowthRates } from '../interfaces/big-five.interface';
+import { CashFlowData, GrowthRates, YearValue } from '../interfaces/big-five.interface';
 import { BaseCalculator } from './base.calculator';
 
 /**
@@ -10,5 +10,17 @@ export class FCFGrowthCalculator extends BaseCalculator {
   /**
    * Calculate FCF growth rates
    */
-  calculate(cashFlow: CashFlowData): GrowthRates {}
+  calculate(cashFlow: CashFlowData): GrowthRates {
+    const years = this.getSortedYears(cashFlow);
+    const values: YearValue[] = [];
+
+    for (const yearStr of years) {
+      const year = this.extractYear(yearStr);
+      const fcf = cashFlow[yearStr]?.FreeCashFlow || 0;
+
+      if (fcf > 0) values.push({ year, value: fcf });
+    }
+
+    return this.calculateGrowthRates(values);
+  }
 }
