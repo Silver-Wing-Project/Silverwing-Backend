b0239a0d587decd2aeddd326ebc315e5fe5a20b2
Tomer <tomermi123@gmail.com>
2025-10-27 12:23:24 +0200

    feat: Implement methods to get Earning per Share (EPS) growth rate

--- src/yahoo-client/analysis/big-five-analysis/calculators/eps-growth.calculator.ts
+++ src/yahoo-client/analysis/big-five-analysis/calculators/eps-growth.calculator.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { BaseCalculator } from './base.calculator';
-import { GrowthRates, IncomeStmtData } from '../interfaces/big-five.interface';
+import { GrowthRates, IncomeStmtData, YearValue } from '../interfaces/big-five.interface';
 
 /**
  * EPS (Earnings Per Share) Growth Calculator
@@ -10,5 +10,17 @@ export class EPSGrowthCalculator extends BaseCalculator {
   /**
    * Calculate EPS growth rates
    */
-  calculate(incomeStmt: IncomeStmtData): GrowthRates {}
+  calculate(incomeStmt: IncomeStmtData): GrowthRates {
+    const years = this.getSortedYears(incomeStmt);
+    const values: YearValue[] = [];
+
+    for (const yearStr of years) {
+      const year = this.extractYear(yearStr);
+      const eps = incomeStmt[yearStr]?.DilutedEPS || 0;
+
+      if (eps > 0) values.push({ year, value: eps });
+    }
+
+    return this.calculateGrowthRates(values);
+  }
 }
