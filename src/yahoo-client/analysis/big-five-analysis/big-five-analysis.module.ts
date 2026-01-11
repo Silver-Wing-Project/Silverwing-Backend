41b151e87799fb532ce5c0460b77bc5b3fd48f82
Tomer <tomermi123@gmail.com>
2026-01-11 20:08:52 +0200

    feat(big-five): implement BigFiveAnalysisModule, to centralize analysis logic, services and calculators.

--- src/yahoo-client/analysis/big-five-analysis/big-five-analysis.module.ts
+++ src/yahoo-client/analysis/big-five-analysis/big-five-analysis.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BigFiveAnalysisService } from './big-five-analysis.service';
 import { BigFiveAnalysisController } from './big-five-analysis.controller';
+import { RoicCalculator } from './calculators/roic.calculator';
+import { BVPSGrowthCalculator } from './calculators/bvps-growth.calculator';
+import { EPSGrowthCalculator } from './calculators/eps-growth.calculator';
+import { FCFGrowthCalculator } from './calculators/fcf-growth.calculator';
+import { SalesGrowthCalculator } from './calculators/sales-growth.calculator';
+import { StockReportModule } from '@/yahoo-client/stock/stock-report/stock-report.module';
+import { PythonModule } from '@/yahoo-client/utility/ts-services/python.module';
 
 @Module({
-  providers: [BigFiveAnalysisService],
+  imports: [StockReportModule, PythonModule],
+  providers: [
+    BigFiveAnalysisService,
+    RoicCalculator,
+    BVPSGrowthCalculator,
+    SalesGrowthCalculator,
+    EPSGrowthCalculator,
+    FCFGrowthCalculator,
+  ],
+  exports: [StockReportModule],
   controllers: [BigFiveAnalysisController],
 })
 export class BigFiveAnalysisModule {}
